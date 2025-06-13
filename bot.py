fc1daa0bad22576bc92000e876b05833d096b611
ArtOptyma <klesh.01081980@gmail.com>
2025-06-14 00:11:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -118,7 +118,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("найди", find_command))
+    app.add_handler(CommandHandler("find", find_command))
     app.add_handler(MessageHandler(filters.LOCATION, handle_location))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
